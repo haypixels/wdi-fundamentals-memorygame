2964b51c3b26273a1410888dc3115b118320e1f7
Midori <m.kameya13@gmail.com>
2018-02-25 12:36:56 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,33 @@
-console.log("Up and running!")
-
-var cards = ["queen","queen","king","king"] ;
-var cardsInPlay = [] ;
-
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-if
-(cardsInPlay.length === 2) {
-
-if (cardsInPlay[0]===cardsInPlay[1]) {
-  alert("You found a match!");
-} else{
-  alert("Sorry,try again.");
- }
-}
+var cards = ['queen', 'queen', 'king', 'king'];
 
 
+var cardsInPlay = [];
+
+
+var checkForMatch = function () {
+
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+};
+
+var flipCard = function (cardId) {
+
+
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
 
 
 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
